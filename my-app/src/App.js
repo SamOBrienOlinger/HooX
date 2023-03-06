525ceedd1d2d22fbbe8a8a6bbc851615fd9f9326
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-06 14:10:56 +0000

    Update Apps.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,16 @@ import Sidebar from "./components/Sidebar";
 import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from "./components/NavBarForm";
 import Content from './components/Content';
+import ControlledFormHooks from './components/ControlledFormHooks';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarForm />
+      {/* <NavBarForm />
       <Sidebar />
-      <Content />
+      <Content /> */}
+      <ControlledFormHooks />
     </div>
   );
 }
